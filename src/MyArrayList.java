2bbab303cf12d82c7746406bf22fdc4d36402083
Jim Lee <bass-guy@cfl.rr.com>
2020-05-09 18:45:13 -0400

    Initial commit for MyArrayList class stub

---
+++ src/MyArrayList.java
@@ -0,0 +1,11 @@
+
+public class MyArrayList {
+
+	public static void main(String[] args) {
+		System.out.println("MyArrayList class is running...");
+		// 1. Describe auto-boxing, including why it is useful.
+		
+		System.out.println("MyArrayList class is done.");
+	}
+
+}
