edbeea3558ff3b067e3741d81b87f76c696342eb
Jim Lee <bass-guy@cfl.rr.com>
2020-05-10 06:38:26 -0400

    Show examples of ArrayList, Collections.sort(), shuffle(), indexOf(), toArray(), and asList()

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class MyArrayList {
 
@@ -35,11 +39,46 @@ public class MyArrayList {
 		
 		// When you chain a series of StringBuilder methods, it applies them from left to right
 		stringBuilder.reverse().delete(8, 11).replace(11, 15, "HTML");
-		System.out.println(stringBuilder.toString());
+		System.out.println(stringBuilder.toString() + "\n");
 
-		// 2. 
+		// 2. ArrayList
 		
-		System.out.println("\nMyArrayList class is done.");
+		List<String> lNames = Arrays.asList("Alex", "Charles", "Brian", "David", "Susan");
+		
+		ArrayList<String> alNames = new ArrayList<String>();
+		
+		alNames.addAll(lNames);
+		
+		for (String item : alNames) {
+			System.out.println(item);
+		}
+		
+		System.out.println(alNames);
+		
+		Collections.sort(alNames);
+		System.out.println(alNames);
+		
+		Collections.shuffle(alNames);
+		System.out.println(alNames);
+		
+		int inxSusan = alNames.indexOf("Susan");
+		System.out.println(inxSusan);
+		
+		int inxUnknown = alNames.indexOf("Unknown");
+		System.out.println(inxUnknown);
+		
+		Object[] arrNames = alNames.toArray();
+		System.out.println(arrNames);
+		for (Object obj : arrNames) {
+            System.out.print(obj + " ");
+		}
+		System.out.println();
+		
+		List<Object> newListNames = Arrays.asList(arrNames);
+		System.out.println(newListNames);
+		
+		
+		System.out.println("\n\nMyArrayList class is done.");
 	}
 
 }
